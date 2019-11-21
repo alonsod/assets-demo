467a4768198532140b4dde7a15c977775e649375
alonsod <d_alonsoc@yahoo.com.mx>
2019-11-21 07:53:23 -0600

    Test de prueba para git

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 
 // ReactDOM.render('Hola React!', document.body)
 
-ReactDOM.render('Hola Administración de Activos. ', document.getElementById('app'))
+ReactDOM.render('Hola Administración de Activos! ', document.getElementById('app'))
 
 // console.log('test de compilación')
